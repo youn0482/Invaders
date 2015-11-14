9c90b28a951a940158270c1482e4bc1cf33309ce
Matt Young <young0482@algonquinlive.com>
2015-11-13 22:33:16 -0500

    initial
    UI Done.

---
+++ Invaders/GamePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Invaders
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            this.InitializeComponent();
+
+
+            Canvas.SetTop(player, Window.Current.Bounds.Height - (player.Height + 20));
+            Canvas.SetLeft(player, Window.Current.Bounds.Width / 2);
+
+
+        }
+
+       
+             private void Grid_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+                {
+                    if (args.VirtualKey == Windows.System.VirtualKey.Right)
+                        Canvas.SetLeft(player, Canvas.GetLeft(player) + 30);
+                    else if (args.VirtualKey == Windows.System.VirtualKey.Left)
+                        Canvas.SetLeft(player, Canvas.GetLeft(player) - 30);
+                }
+    
+    }
+}
---
+++ Invaders/Settings.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Invaders
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Settings : Page
+    {
+        public Settings()
+        {
+            this.InitializeComponent();
+        }
+
+        private void settingsBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void howBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(HowToPlay));
+        }
+
+        private void aboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(About));
+        }
+    }
+}
